dc7af3790ce72870da55c5c7bfeb0b101377f7da
Nhan Nguyen <nhantnguyen42@gmail.com>
2023-01-05 18:11:57 -0800

    Finished up to 3.8.

--- phonebook/index.js
+++ phonebook/index.js
@@ -1,6 +1,24 @@
 const express = require("express");
 const app = express();
+const morgan = require("morgan");
 app.use(express.json());
+morgan.token("body", function (req, res) {
+  return JSON.stringify(req.body);
+});
+app.use(
+  morgan(function (tokens, req, res) {
+    return [
+      tokens.method(req, res),
+      tokens.url(req, res),
+      tokens.status(req, res),
+      tokens.res(req, res, "content-length"),
+      "-",
+      tokens["response-time"](req, res),
+      "ms",
+      tokens.body(req, res),
+    ].join(" ");
+  })
+);
 
 let people = [
   {
@@ -27,10 +45,71 @@ let people = [
 
 app.get("/api/persons", (req, res) => {
   res.json(people);
-})
+});
+
+app.get("/api/persons/:id", (req, res) => {
+  const id = Number(req.params.id);
+  const person = people.find((person) => person.id === id);
+  if (person) {
+    res.json(person);
+  } else {
+    res.status(404).end();
+  }
+});
+
+app.post("/api/persons", (req, res) => {
+  const body = req.body;
+
+  if (!body.name || !body.number) {
+    return res
+      .status(400)
+      .json({
+        error: "name and/or number missing",
+      })
+      .end();
+  } else if (people.find((person) => person.name === body.name)) {
+    return res
+      .status(409)
+      .json({
+        error: "name must be unique",
+      })
+      .end();
+  } else {
+    const person = {
+      id: Math.floor(Math.random() * 500),
+      name: body.name,
+      number: body.number,
+    };
+
+    people = people.concat(person);
+    res.json(person);
+  }
+});
+
+app.delete("/api/persons/:id", (req, res) => {
+  const id = Number(req.params.id);
+  const person = people.find((person) => person.id === id);
+  if (person) {
+    people = people.filter((person) => person.id !== id);
+    res.json({
+      success: "person removed",
+    });
+  } else {
+    res.status(404).json({
+      error: "person already removed",
+    });
+  }
+});
+
+app.get("/api/info", (req, res) => {
+  res.send(
+    `<p>Phonebook has info for ${people.length} people</p>
+    <p>${new Date()}</p>`
+  );
+});
 
 const PORT = 3001;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
-})
+});
